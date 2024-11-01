960e87d29c0acd7dd4e3b426a58988c489113384
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-01 15:40:08 +0100

    main

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
-import Main from './components/MainContent'
+import MainContent from './components/MainContent'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
         <Header/>
-        <Main />
-        <WelcomeMessage />
+        <MainContent/>
+        <WelcomeMessage/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
