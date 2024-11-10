c38b54f42668a2ed3e4acf3748ec72585a415ee5
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-10 19:58:43 +0100

    Create ProfilePage.js

---
+++ alx-react-app-props/src/components/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+
+function ProfilePage() {
+    return (
+        <div>
+            <h2>Profile</h2>
+            <UserDetails />
+        </div>
+    );
+}
+
+export default ProfilePage;
