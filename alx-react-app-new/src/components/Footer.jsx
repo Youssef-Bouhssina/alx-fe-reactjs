db195c325b6f77c2db41ca2e893098c469d9ce05
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-10 14:53:56 +0100

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>
+        <footer style={{backgroundColor: 'navy', color: 'white', textAlign: 'center'}}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
