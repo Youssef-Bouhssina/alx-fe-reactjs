ba1025c9a2cd16efa5c8ad9b6944d3cd0004ba3c
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-17 13:40:37 +0100

    Revert "Update recipeStore.js"
    This reverts commit e647f5499b0a9765ba33dd1812f05abe81a3d15b.

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,27 +1,8 @@
-import create from 'zustand';
+import create from 'zustand'
 
-const useRecipeStore = create((set) => ({
+const useRecipeStore = create(set => ({
     recipes: [],
-
-    // Add a new recipe
-    addRecipe: (newRecipe) =>
-        set((state) => ({
-            recipes: [...state.recipes, newRecipe],
-        })),
-
-    // Delete a recipe by ID
-    deleteRecipe: (id) =>
-        set((state) => ({
-            recipes: state.recipes.filter((recipe) => recipe.id !== id),
-        })),
-
-    // Update a recipe by ID
-    updateRecipe: (id, updatedData) =>
-        set((state) => ({
-            recipes: state.recipes.map((recipe) =>
-                recipe.id === id ? { ...recipe, ...updatedData } : recipe
-            ),
-        })),
+    addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+    setRecipes: (recipes) => set({ recipes })
 }));
-
-export default useRecipeStore;
+export default { useRecipeStore };
