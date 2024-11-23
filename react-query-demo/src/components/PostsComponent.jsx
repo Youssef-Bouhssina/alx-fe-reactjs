029a53c6f5aa7370a671bbef8444776e5dba1444
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-23 05:58:44 +0100

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,18 +1,22 @@
 import { useQuery } from 'react-query';
 
 function PostsComponent() {
-    const { isLoading, error, data, isFetching, refetch } = useQuery('posts', () =>
-        fetch('https://jsonplaceholder.typicode.com/posts').then((response) =>
-            response.json()
-        )
+    const { isLoading, error, data, isFetching, refetch } = useQuery(
+        'posts',
+        fetchPosts
     );
+
+    const fetchPosts = () => {
+        return fetch('https://jsonplaceholder.typicode.com/posts').then((response) =>
+            response.json()
+        );
+    };
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
 
     return (
         <div>
-            {/* Button to trigger refetch */}
             <button onClick={refetch} disabled={isFetching}>
                 {isFetching ? 'Refetching...' : 'Refetch Posts'}
             </button>
