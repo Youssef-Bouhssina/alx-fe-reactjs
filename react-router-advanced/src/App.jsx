a9359bc78f4e89591021a0091fa1d19212a52427
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-24 01:28:29 +0100

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
                     <Route path="details" element={<ProfileDetails />} />
                     <Route path="settings" element={<ProfileSettings />} />
                 </Route>
-                <Route path="/blog/:postId" element={<BlogPost />} />
+                <Route path="/blog/:id" element={<BlogPost />} /> {/* Updated */}
             </Routes>
         </BrowserRouter>
     );
