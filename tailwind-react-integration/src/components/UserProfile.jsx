139bfe97c34713746778e446e54d07b1668060f3
Youssef-Bouhssina <ysf.bouhssina@gmail.com>
2024-11-28 16:31:37 +0100

    WM.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
         <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg
-                        md:max-w-sm p-4 md:max-w-sm p-8">
+                        sm:p-4 md:p-8">
             <img src="https://via.placeholder.com/150" alt="User"
                  className="rounded-full w-36 h-36 mx-auto)"/>
             <h1 className="text-xl text-blue-800 my-4
